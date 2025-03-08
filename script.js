b813a3cb391be90b550e69e3282be75505aa4d46
giharapathi <tharusamarakkody@gmail.com>
2025-03-09 02:48:48 +0530

    new changes

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["an IT Undergraduate", "a Software Engineering", "a UI/UX Designer", "a Logo Designer", "a Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["and IT Undergraduate", "a Software Engineering", "a UI/UX Designer", "a Logo Designer", "a Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
